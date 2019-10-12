3fa8348c43d0ff5530d85bf3d1d92b32bf466910
Alok Tiwari <44957600+aloktiwari123@users.noreply.github.com>
2019-10-13 00:47:58 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -1,6 +1,1 @@
-1.	Name three parameters of the carrier that can be varied to produce modulation.
-2.	Define amplitude modulation
-3.	Is the signal modulated when it transmits through a baseband communication system?
-4.	Define modulation index.
-
 
