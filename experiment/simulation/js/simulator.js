604789a5018122603385727cab3a26bbbc9207ce
Alok Tiwari <44957600+aloktiwari123@users.noreply.github.com>
2019-10-17 13:39:55 +0530

    Update simulator.js

--- experiment/simulation/js/simulator.js
+++ experiment/simulation/js/simulator.js
@@ -1,33 +1,86 @@
 (function() {
   const myQuestions = [
     {
-      question: "Who is the strongest?",
+      question: "1.The circuit used for producing AM is called?",
       answers: {
-        a: "Superman",
-        b: "The Terminator",
-        c: "Waluigi, obviously"
+        a: " Modulator",
+        b: " Transmitter ",
+        c: "Receiver",
+        d: " Duplexer ",
       },
-      correctAnswer: "c"
+      correctAnswer: "a"
     },
     {
-      question: "What is the best site ever created?",
+      question: "2.In Amplitude Modulation, the instantaneous values of the carrier amplitude changes in accordance with the amplitude and frequency variations of the modulating signal.",
       answers: {
-        a: "SitePoint",
-        b: "Simple Steps Code",
-        c: "Trick question; they're both the best"
+        a: "True",
+        b: "False",
+        
       },
-      correctAnswer: "c"
+      correctAnswer: "a"
     },
     {
-      question: "Where is Waldo really?",
+      question: "3.What is the line connecting the positive and negative peaks of the carrier waveform called? ",
       answers: {
-        a: "Antarctica",
-        b: "Exploring the Pacific Ocean",
-        c: "Sitting in a tree",
-        d: "Minding his own business, so stop asking"
+        a: "Peak line",
+        b: "Maximum amplitude ceiling ",
+        c: "Modulation index",
+        d: " Envelope "
       },
       correctAnswer: "d"
-    }
+    },
+     {
+      question: "4.The ratio between the modulating signal voltage and the carrier voltage is called? ",
+      answers: {
+        a: "Amplitude modulation",
+        b: "Modulation frequency ",
+        c: "Modulation index",
+        d: "Ratio of modulation  "
+      },
+      correctAnswer: "c"
+    },
+     {
+      question: "5.What happens when the amplitude of the modulating signal is greater than the amplitude of the carrier? ",
+      answers: {
+        a: "Decay ",
+        b: "Distortion ",
+        c: "Amplification",
+        d: "Attenuation"
+      },
+      correctAnswer: "b"
+     },
+     {
+      question: "6.What is the condition for greatest output power at the transmitter without distortion? ",
+      answers: {
+        a: "Modulating signal voltage > Carrier voltage",
+        b: " Modulating signal voltage < Carrier voltage  ",
+        c: "Modulating signal voltage = Carrier voltage ",
+        d: "Modulating signal voltage = 0"
+      },
+      correctAnswer: "c"
+     },
+     {
+      question: "7.What is the bandwidth of the signal if the highest frequency in the modulating signal is 3 KHz? ",
+      answers: {
+        a: "6Khz",
+        b: "3KHz ",
+        c: " 5Khz ",
+        d: "140Khz "
+      },
+      correctAnswer: "a"
+     },
+    {
+      question: "8.What is the wave of frequency that occurs at the sum and difference of the carrier and modulating signal is called? ",
+      answers: {
+        a: " Noise signals ",
+        b: "Sideband ",
+        c: "Extraband ",
+        d: "Neutral band  "
+      },
+      correctAnswer: "b"
+     },
+    
+    
   ];
 
   function buildQuiz() {
